f567488bb5c6a104848bc67c63d4cca8e5e137fe
liang2711 <1547567910@qq.com>
2024-02-22 01:41:36 +0800

    new code

--- app/src/main/java/com/example/accessibilityt/xposed/ConstantX.java
+++ app/src/main/java/com/example/accessibilityt/xposed/ConstantX.java
@@ -3,9 +3,12 @@ package com.example.accessibilityt.xposed;
 import android.net.Uri;
 
 public class ConstantX {
-    public static final String SERVICER_IP="http://192.168.169.1:8088/dex2java";
+    public static final String APPLICATION_JSON="/data/user/0/com.example.accessibilityt/cache/application.json";
+    public static final String SERVICER_IP="http://192.168.1.41:8088/dex2java";
     public static final String KEY_PACKAGENAME="packagename";
     public static final String KEY_CLASSNAME="classname";
+
+    public static final String KET_APPNAME="appname";
 
     private static final String AUTOHORITY="com.example.infoprovider";
     private static final String KEY_NAME="xposed";
