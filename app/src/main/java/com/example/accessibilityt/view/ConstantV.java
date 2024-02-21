4e813c4648e78ba0a1e021b555c31bc854b435f6
liang2711 <1547567910@qq.com>
2024-02-22 01:41:29 +0800

    new code

--- app/src/main/java/com/example/accessibilityt/view/ConstantV.java
+++ app/src/main/java/com/example/accessibilityt/view/ConstantV.java
@@ -3,8 +3,11 @@ package com.example.accessibilityt.view;
 import android.net.Uri;
 
 public class ConstantV {
-    public static final String SERVICER_IP="http://192.168.169.1:8088/dex2java";
+    public static final String APPLICATION_JSON="/data/user/0/com.example.accessibilityt/cache/application.json";
+    public static final String SERVICER_IP="http://192.168.1.41:8088/dex2java";
     public static final String KEY_NAME="xposed";
+
+    public static final String KET_APPNAME="appname";
 
     public static final int ITEM=1;
 
